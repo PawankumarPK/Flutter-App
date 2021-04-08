8d38aee473877b1db26486fbf29435d70a403b2a
PawankumarPK <pk9910765616@gmail.com>
2021-04-09 00:02:54 +0530

    button added with ripple effect and padding to all textinput field

--- lib/main.dart
+++ lib/main.dart
@@ -56,29 +56,44 @@ class _LoginPageState extends State<LoginPage>
               new FlutterLogo(
                 size: _iconAnimation.value * 100,
               ),
-              new Form(child: new Theme(data: new ThemeData(
-                brightness: Brightness.dark,
-                primarySwatch: Colors.teal,
-                inputDecorationTheme: new InputDecorationTheme(
-                  labelStyle: new TextStyle(
-                    color: Colors.teal,fontSize: 20.0
-                  )
-                )
-              ), child: Column(
-                children: <Widget>[
-                  new TextField(
-                    decoration: new InputDecoration(
-                      hintText: "Enter Email"
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  new TextField(
-                    decoration: new InputDecoration(
-                      hintText: "Enter Password"
-                    ),
-                  )
-                ],
-              )))
+              new Form(
+                  child: new Theme(
+                      data: new ThemeData(
+                          brightness: Brightness.dark,
+                          primarySwatch: Colors.teal,
+                          inputDecorationTheme: new InputDecorationTheme(
+                              labelStyle: new TextStyle(
+                                  color: Colors.teal, fontSize: 20.0))),
+                      child: Container(
+                          padding: const EdgeInsets.all(40.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              new TextFormField(
+                                decoration: new InputDecoration(
+                                    labelText: "Enter Email"),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              new TextFormField(
+                                decoration: new InputDecoration(
+                                    labelText: "Enter Password"),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                              ),
+                              new Padding(
+                                padding: const EdgeInsets.only(top: 20.0),
+                              ),
+                              new MaterialButton(
+                                height: 40.0,
+                                minWidth: 100.0,
+                                color: Colors.teal,
+                                textColor: Colors.white,
+                                child: new Text("Login Now"),
+                                onPressed: () => {},
+                                splashColor: Colors.redAccent,
+                              )
+                            ],
+                          ))))
             ],
           )
         ],
